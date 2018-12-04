6270864ec7874bf3f68d2aa898ba7782a93bea78
Kris Widy. Febyanti <kwf2102@gmail.com>
2018-12-05 06:42:12 +0700

    First

---
+++ app/src/main/java/com/example/krisandroid/noteme/AddMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.krisandroid.noteme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_main);
+    }
+}
