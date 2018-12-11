8606185bfe93004b40686fb7757cfdec23581840
Kris Widy. Febyanti <kwf2102@gmail.com>
2018-12-11 23:13:18 +0700

    logo

--- app/src/main/java/com/example/krisandroid/noteme/Rest/ApiClient.java
+++ app/src/main/java/com/example/krisandroid/noteme/Rest/ApiClient.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class ApiClient {
-    public static final String BASE_URL = "http://192.168.43.120/noteme_server/index.php/";
+    public static final String BASE_URL = "http://192.168.0.108/noteme_server/index.php/";
     private static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit==null) {
